dc3ffa37dc876282d66c849d4ed7c566b6332f5d
lduehr <2025duehr.lucas@fcboe.org>
2024-01-24 13:42:00 -0500

    Completed assignment

---
+++ c/scanf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int age;
+	float gpa;
+	char grade;
+	printf ("enter your age: ");
+	scanf ("%i", &age);
+
+	printf("your gpa now: ");
+	scanf (" %f", &gpa);
+
+	printf("your leter grade: ");
+	scanf (" %c", &grade);
+
+	return 0;
+}
